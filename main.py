cb8e906d3e558f9d4e1e37f3cc35058faf23af01
adityakusingh <150362352+adityakusingh@users.noreply.github.com>
2023-11-10 10:17:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -68,3 +68,39 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
+
+
+#webcam
+
+cap = cv2.VideoCapture(1)
+
+if not cap.isOpened():
+  cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+   raise IOError("Can't open the video")
+
+font_scale =3
+font = cv2.FONT_HERSHEY_PLAIN
+
+while True:
+  ret,frame = cap.read()
+
+  ClassIndex, confidece, bbox = model.detect(frame, confThreshold = 0.55)
+
+  print(ClassIndex)
+
+  if(len(ClassIndex)!=0):
+    for ClassInd,  conf, boxes in zip(ClassIndex.flatten(), confidece.flatten(), bbox):
+      if(ClassInd<=80):
+        cv2.rectangle(img, boxes,(255,0,0),2)
+        cv2.putText(img, classLabels[ClassInd-1], (boxes[0]+10,boxes[1]+40), font, fontScale = font_scale, color=(0,255,0),thickness=3)
+
+
+  cv2_imshow(frame)
+
+  if cv2.waitKey(2) & 0xff == ord('q'):
+    break
+
+cap.release()
+cv2.destroyAllWindows()
